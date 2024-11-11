690b4d58b32cd65d027da92d7ce670098c54e372
fuentestomas <fuentestomas1406@gmail.com>
2024-11-11 02:38:28 -0300

    fab button en home

--- ui/src/app/tab2/tab2.page.ts
+++ ui/src/app/tab2/tab2.page.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonCardSubtitle, IonIcon, IonFab, IonFabButton, IonLabel } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { addIcons } from 'ionicons';
+import { add } from 'ionicons/icons';
 import { register } from 'swiper/element/bundle';
 
 register();
@@ -11,7 +13,7 @@ register();
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonSearchbar, IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonCardSubtitle],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonSearchbar, IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonCardSubtitle, IonIcon,  IonFab, IonFabButton, IonLabel],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 
@@ -23,6 +25,8 @@ export class Tab2Page {
     centeredSlides: true
   };
 
-  constructor() {}
+  constructor() {
+    addIcons({ add });
+  }
 
 }
