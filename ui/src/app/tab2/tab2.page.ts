90d663fb33fa3582a3497697a6352068e9374c3f
fuentestomas <fuentestomas1406@gmail.com>
2024-11-11 00:51:02 -0300

    avance home

--- ui/src/app/tab2/tab2.page.ts
+++ ui/src/app/tab2/tab2.page.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar } from '@ionic/angular/standalone';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonSearchbar]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonSearchbar, IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonCardSubtitle],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
+
 export class Tab2Page {
+
+  slideOpts = {
+    slidesPerView: 1.2,
+    spaceBetween: 10,
+    centeredSlides: true
+  };
 
   constructor() {}
 
