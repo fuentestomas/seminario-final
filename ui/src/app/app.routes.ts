7be59a33cdc6eb75ee34eaf497c38da57191b3a9
fuentestomas <fuentestomas1406@gmail.com>
2024-11-11 00:46:43 -0300

    librerias y ruta base

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -5,4 +5,12 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'search',
+    loadComponent: () => import('./search/search.page').then( m => m.SearchPage)
+  },
+  {
+    path: 'calendar',
+    loadComponent: () => import('./calendar/calendar.page').then( m => m.CalendarPage)
+  },
 ];
